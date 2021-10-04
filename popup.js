342a3e89573f5be6704e0e82af9805d8446635b3
John Alber Santos <santos1802909@ceu.edu.ph>
2021-10-04 10:29:36 +0800

    syle: Replaced image

--- popup.js
+++ popup.js
@@ -17,7 +17,7 @@ function spawnimage(x, y) {
   lastY = y;
 
   var image = document.createElement('img');
-  image.setAttribute('src', './circle.png');
+  image.setAttribute('src', './polka.png'); // Replaced circle.png to polka.png
   image.setAttribute('class', 'overlays');
   image.setAttribute('width', 128);
   image.setAttribute('height', 128);
