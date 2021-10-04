5014200039a7b5cd6638bf0ebdb1432e7bc77072
Jose Carlo Olaer <olaer1801309@ceu.edu.ph>
2021-10-04 14:57:16 +0700

    Position Change

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@
 let images = [];
 
 // Store mouse position
-let mousePos = { x: -1000, y: -1000};
+let mousePos = { x: -5000, y: -1000};
 
-let lastX = -1000;
-let lastY = -1000;
+let lastX = -2000;
+let lastY = -3000;
 
 function spawnimage(x, y) {
   if (lastX === x && lastY === y) {
