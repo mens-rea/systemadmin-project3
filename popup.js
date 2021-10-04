d0f499b27e42665f7e69e0ef026e68e9f577e5f7
Louie Cabral <louiealdrin25@gmail.com>
2021-10-04 16:15:21 +0800

    Changed Ball Starting Position

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@
 let images = [];
 
 // Store mouse position
-let mousePos = { x: -1000, y: -1000};
+let mousePos = { x: -120, y: -120};
 
-let lastX = -1000;
-let lastY = -1000;
+let lastX = -120;
+let lastY = -120;
 
 function spawnimage(x, y) {
   if (lastX === x && lastY === y) {
