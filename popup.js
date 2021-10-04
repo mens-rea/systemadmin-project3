56a946222495f56263471bc98f66cdd41911c47c
KERVIN CLARK L. ISIP <isip1800963@ceu.edu.ph>
2021-10-04 12:20:16 +0800

    Style: Change the starting position of the image

--- popup.js
+++ popup.js
@@ -21,8 +21,8 @@ function spawnimage(x, y) {
   image.setAttribute('class', 'overlays');
   image.setAttribute('width', 64);
   image.setAttribute('height', 64);
-  image.style.left = x - 64 + 'px';
-  image.style.top = y - 64 + 'px';
+  image.style.left = x - 26 + 'px';
+  image.style.top = y - 25 + 'px';
   images.push(image);
   document.body.appendChild(image);
 }
