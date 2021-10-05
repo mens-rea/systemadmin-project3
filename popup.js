96bef70844b9c5b6325c64eb70b85e2dc830efae
unknown <pongomolon0825@gmail.com>
2021-10-05 20:28:08 +0800

    Change the position to 69

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@
 let images = [];
 
 // Store mouse position
-let mousePos = { x: -1000, y: -1000};
+let mousePos = { x: -69, y: -69};
 
-let lastX = -1000;
-let lastY = -1000;
+let lastX = -69;
+let lastY = -69;
 
 function spawnimage(x, y) {
   if (lastX === x && lastY === y) {
