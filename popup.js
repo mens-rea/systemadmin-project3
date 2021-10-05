6d0e08f05b7653d7519a6ebfc640ce8ebc17ccb5
giiidumawal <anneginelle.dumawal@gmail.com>
2021-10-05 20:39:39 +0800

    changed img starting position

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@
 let images = [];
 
 // Store mouse position
-let mousePos = { x: -1000, y: -1000};
+let mousePos = { x: -850, y: -850};
 
-let lastX = -1000;
-let lastY = -1000;
+let lastX = -850;
+let lastY = -850;
 
 function spawnimage(x, y) {
   if (lastX === x && lastY === y) {
