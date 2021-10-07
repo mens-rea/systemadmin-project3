5d43988af3a29c24118ba97e5cd3f709daab88c2
Ralph Ramirez <56585730+Ramirez999@users.noreply.github.com>
2021-10-07 22:09:33 +0900

    style: change position

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@
 let images = [];
 
 // Store mouse position
-let mousePos = { x: -1000, y: -1000};
+let mousePos = { x: -500, y: -500};
 
-let lastX = -1000;
-let lastY = -1000;
+let lastX = -500;
+let lastY = -500;
 
 function spawnimage(x, y) {
   if (lastX === x && lastY === y) {
@@ -21,8 +21,8 @@ function spawnimage(x, y) {
   image.setAttribute('class', 'overlays');
   image.setAttribute('width', 64);
   image.setAttribute('height', 64);
-  image.style.left = x - 64 + 'px';
-  image.style.top = y - 64 + 'px';
+  image.style.left = x - 32 + 'px';
+  image.style.top = y - 32 + 'px';
   images.push(image);
   document.body.appendChild(image);
 }
