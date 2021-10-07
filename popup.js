c4343f7d6d86e57e3c575eec190358d142c9f078
seleneartemis <reesenicolerivera@gmail.com>
2021-10-07 12:05:08 +0800

    set a different starting position for image

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@
 let images = [];
 
 // Store mouse position
-let mousePos = { x: -1000, y: -1000};
+let mousePos = { x: -300, y: -300};
 
-let lastX = -1000;
-let lastY = -1000;
+let lastX = -300;
+let lastY = -300;
 
 function spawnimage(x, y) {
   if (lastX === x && lastY === y) {
