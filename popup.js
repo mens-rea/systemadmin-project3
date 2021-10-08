92b495f2a24a95758c56ad3aaf280ff50a41fdd3
Cloud16 <caringal1820662@mkt.ceu.edu.ph>
2021-10-08 18:02:25 +0800

    pic positions

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@
 let images = [];
 
 // Store mouse position
-let mousePos = { x: -1000, y: -1000};
+let mousePos = { x: -700, y: -700};
 
-let lastX = -1000;
-let lastY = -1000;
+let lastX = -700;
+let lastY = -700;
 
 function spawnimage(x, y) {
   if (lastX === x && lastY === y) {
